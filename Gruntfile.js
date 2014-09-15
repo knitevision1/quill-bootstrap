892168f8b5705e6a74cd63db2803dc606fe15dd3
knitevision1 <shvld22.dev@gmail.com>
2014-09-15 22:37:52 +0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+
+    includereplace: {
+		options: {
+	    },
+	    dist: {
+	      src: '*.html',
+	      dest: 'build/'
+	    }
+	   },
+
+    less: {
+      dist: {
+        files: {
+          'build/css/bootstrap.css': 'sources/less/bootstrap.less',
+          'build/css/custom.css': 'sources/less/custom.less',
+        }
+      }
+    },
+	
+    watch: {
+      css: {
+        files: ['sources/less/*.less', 'sources/mixins/*.less', 'sources/less/custom*.less'],
+        tasks: ['less'],
+      },
+      includereplace: {
+        files: [ '*.html', 'includes/*.html' ],
+        tasks: ['includereplace']
+	  }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-include-replace');
+  grunt.registerTask('default', ['includereplace', 'less', 'watch']);
+  grunt.registerTask('production', ['includereplace', 'less', 'watch']);
+}
