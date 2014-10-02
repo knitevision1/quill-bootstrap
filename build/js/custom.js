5275166dfbcd541c817968025307604a8c55d818
knitevision1 <shvld22.dev@gmail.com>
2014-10-03 00:24:47 +0300

    Commited the select boxes that look the same on all the devices

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,4 +1,87 @@
+$(document).ready(function(){
+
+	//1st tab
+
+	$("#classroom-select").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-white",
+		classSelector: "select-box-trigger"
+	});
+
+	$("#units-select").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-gray",
+		classOptions: "select-white",
+		classSelector: "select-box-trigger",
+		classDisabled: "select-box-disabled",
+		classHolderDisabled: "select-gray"
+	});
+
+
+	//2nd tab
+
+	$("#classroom-select-2").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-white",
+		classSelector: "select-box-trigger"
+	});
+
+	$("#units-select-2").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-gray",
+		classOptions: "select-white",
+		classSelector: "select-box-trigger",
+		classDisabled: "select-box-disabled",
+		classHolderDisabled: "select-gray"
+	});
+
+
+
+	//3rd tab
+
+	$("#classroom-select-3").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-white",
+		classSelector: "select-box-trigger"
+	});
+
+	$("#units-select-3").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-gray",
+		classOptions: "select-white",
+		classSelector: "select-box-trigger",
+		classDisabled: "select-box-disabled",
+		classHolderDisabled: "select-gray"
+	});
+
+
+
+	//4th tab
+
+	$("#classroom-select-4").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-white",
+		classSelector: "select-box-trigger"
+	});
+
+	$("#units-select-4").selectbox({
+		classToggle: "fa fa-caret-down",
+		classHolder: "select-mixin select-custom-mixin select-gray",
+		classOptions: "select-white",
+		classSelector: "select-box-trigger",
+		classDisabled: "select-box-disabled",
+		classHolderDisabled: "select-gray"
+	});
+
+});
+
+
+
+$("#units-select").selectbox("enable")
+
 $('#tooltip-test').tooltip({
 	html: 'true'
 	}
 );
+	
+
